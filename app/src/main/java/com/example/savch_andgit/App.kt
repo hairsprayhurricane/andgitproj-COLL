3ef233783afdb70f51444aa68216f6bade3aaf37
hairsprayhurricane <truediamondjake@gmail.com>
2025-11-20 12:29:03 +0300

    removed unnecessary language files
    and padding fix

--- app/src/main/java/com/example/savch_andgit/App.kt
+++ app/src/main/java/com/example/savch_andgit/App.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.Context
 import android.content.res.Configuration
 import com.example.savch_andgit.di.appModule
-import com.example.savch_andgit.music.utils.LocaleDetector
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -18,12 +17,4 @@ class App : Application() {
         }
     }
 
-    override fun attachBaseContext(base: Context) {
-        super.attachBaseContext(LocaleDetector.setLocale(base))
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        LocaleDetector.setLocale(this)
-    }
 }
